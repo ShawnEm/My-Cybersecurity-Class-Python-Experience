d5a9adb503fbaf7cfc0eae83d9fb4bf89addd271
Shawn Emile <shawn.emile18@gmail.com>
2024-08-26 11:40:38 -0400

    Update Shawn Emile - Ch. 9-Sets Lab Question 1.py

--- Python x Visual Studio/Chapter 9-Sets Labs/Shawn Emile - Ch. 9-Sets Lab Question 1.py
+++ Python x Visual Studio/Chapter 9-Sets Labs/Shawn Emile - Ch. 9-Sets Lab Question 1.py
@@ -2,7 +2,7 @@ Fb = {"Burrow", "Adams", "Polamalu"}
 S = {"Santos", "Ronaldo", "Messi"}
 print(f"Say hello to the football players {Fb}")
 print(f"Say hello to the soccer players {S}")
-print("We will be combing these teams using the Union method")
+print("We will be combining these teams using the Union method")
 NewTeam = Fb.union(S)
 print(NewTeam)
 print("Let's show the difference between soccer from football players")
